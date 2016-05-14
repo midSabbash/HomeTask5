bb12228388a7ae7e525f94c85195e6de9f808a45
midSabbash <Sabbash23@Gmail.com>
2016-05-14 19:31:16 +0300

    1.1

--- src/main/java/ExerciseOne.java
+++ src/main/java/ExerciseOne.java
@@ -2,20 +2,44 @@ import java.io.*;
 
 public class ExerciseOne {
     public static void main(String[] args) throws IOException {
-        File file = new File("test.txt");
 
-        System.out.println(reader(file));
+        String file1 = reader("test.txt");
+        String out = upperCase(file1);
+        System.out.println(out);
     }
 
-    private static String reader(File file) throws IOException {
+    private static String reader(String file) throws IOException {
         FileReader reader = new FileReader(file);
         String str = "";
         int symbol;
         while((symbol = reader.read()) != -1) {
             str += (char) symbol;
         }
+
         str=str.trim();
+        str=str.concat(",");
+        str=str.replace(' ', ',');
+        str=str.replace(':',',');
         return str;
     }
 
+    private static String upperCase(String str) {
+        String intactStr = "", strUp;
+        int quantity;
+        while (str.length() > 0) {
+            quantity = str.indexOf(',');
+
+            if(quantity > 2){
+                strUp = str.substring(0, quantity);
+                strUp = strUp.toUpperCase();
+                intactStr = intactStr.concat(strUp + " ");
+
+            }else
+                intactStr = intactStr.concat(str.substring(0, quantity) + " ");
+
+            str = str.substring(quantity +1);
+
+        }
+        return intactStr;
+    }
 }
